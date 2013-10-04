69e50750d3a29265f4f38f19963b902736738bcd
shirst200 <shirst200@caledonian.ac.uk>
2013-10-04 12:10:04 +0100

    Exercise 1. b)

---
+++ GP2BaseCode/Renderer/Renderer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class IRenderer
+{
+public:
+	virtual ~IRenderer(){};
+	virtual bool init(void *pWindowHandle,bool fullScreen)=0;
+	virtual void clear(float r,float g,float b,float a)=0;
+	virtual void present()=0;
+	virtual void render()=0;
+};
