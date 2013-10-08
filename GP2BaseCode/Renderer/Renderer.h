63e164a7794b951f9efe021e9d1476f400dab593
shirst200 <shirst200@caledonian.ac.uk>
2013-10-08 09:32:27 +0100

    D3D10 exersize2. d)

--- GP2BaseCode/Renderer/Renderer.h
+++ GP2BaseCode/Renderer/Renderer.h
@@ -1,11 +1,13 @@
 #pragma once
 
+//Interface class
 class IRenderer
 {
 public:
+	//Always has to have an empty deconstrcutor
 	virtual ~IRenderer(){};
+	//pure virtual functions have to be implemented
 	virtual bool init(void *pWindowHandle,bool fullScreen)=0;
 	virtual void clear(float r,float g,float b,float a)=0;
 	virtual void present()=0;
-	virtual void render()=0;
 };
